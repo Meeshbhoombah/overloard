8a16d3dd1a63eca83494d13c4f7a947e0041b6bf
Meeshbhoombah <rohanmishra2017@gmail.com>
2023-05-15 18:43:06 +0530

    Added Employee fields and table name

--- src/main/java/demo/overlord/entity/Employee.java
+++ src/main/java/demo/overlord/entity/Employee.java
@@ -2,15 +2,30 @@ package demo.overlord.entity;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 
 @Entity
+@Table(name = "employees")
 public class Employee {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+
+
+    private String firstName;
+    private String lastName;
+
+    private String password;
+
+    private String email;
+    private String phoneNumber;
+
+    private String address;
+
 }
 
