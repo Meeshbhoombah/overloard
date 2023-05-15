c5409399a846ddb6b7f02d6e49bec05b2ae71a47
Meeshbhoombah <rohanmishra2017@gmail.com>
2023-05-15 21:12:39 +0530

    Added setters and getters for variables

--- src/main/java/demo/overlord/entity/Employee.java
+++ src/main/java/demo/overlord/entity/Employee.java
@@ -40,5 +40,80 @@ public class Employee {
         this.email = email;
         this.password = password; 
     }
+
+
+    public long getId() {
+        return id; 
+    }
+
+
+    public void setEmail(String email) {
+        this.email = email; 
+    }
+
+
+    public String getEmail() {
+        return email; 
+    }
+
+    public void setPassword(String password) {
+        this.password = password; 
+    }
+
+    public String getPassword() {
+        return password; 
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber; 
+    }
+
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName; 
+    }
+
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName; 
+    }
+
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role; 
+    }
+
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Date getBirthday() {
+        return birthday; 
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address; 
+    }
 }
 
