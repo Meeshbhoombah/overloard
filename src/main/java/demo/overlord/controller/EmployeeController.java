8b25b51ff523b943aabcbbf415b4a6f0ae96aed8
Meeshbhoombah <rohanmishra2017@gmail.com>
2023-05-15 22:15:53 +0530

    Added Employee controller boilerplate

---
+++ src/main/java/demo/overlord/controller/EmployeeController.java
@@ -0,0 +1,15 @@
+package demo.overlord.controller;
+
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import demo.overlord.repository.EmployeeRepository;
+
