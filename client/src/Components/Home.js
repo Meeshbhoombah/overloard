3a3054f25f1c012b137e622152b3896b4799899a
Meeshbhoombah <rohanmishra2017@gmail.com>
2023-05-16 16:13:29 +0530

    Added form inputs

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -7,6 +9,47 @@ import Form from 'react-bootstrap/Form';
 
 
 export default function Home() {
+    // REGISTER
+    const [registerEmail, setRegisterEmail] = useState("");
+    const [registerPassword, setRegisterPassword] = useState("");
+    const [registerPhoneNumber, setRegisterPhoneNumber] = useState("");
+
+    const [registerFirstName, setRegisterFirstName] = useState("");
+    const [registerLastName, setRegisterLastName] = useState("");
+
+    const [registerRole, setRegisterRole] = useState("");
+
+    const [registerBirthday, setRegisterBirthday] = useState("");
+    const [registerAddress, setRegisterAddress] = useState("");
+
+    let register = async (e) => {
+        e.preventDefault();
+            try {
+                let res = await fetch("/employees", {
+                    method: "POST",
+                    body: JSON.stringify({
+                        email: registerEmail,
+                        password: registerPassword,
+                        phoneNumber: registerPhoneNumber,
+                        firstName: registerFirstName,
+                        lastName: registerLastName,
+                        role: registerRole,
+                        birthday: registerBirthday,
+                        address: registerAddress
+                    }),
+                });
+                let resJson = await res.json();
+                if (res.status === 200) {
+                    // TODO: navigate to dashboard
+                } else {
+                    // TODO: Inform user of error
+                }
+            } catch (err) {
+                console.error(err);
+            }
+    };
+
+
     return (
         <Container>
             <Row>
@@ -16,6 +59,7 @@ export default function Home() {
                 </Col>
             </Row>
             <Row>
+                {/* LOGIN */}
                 <Col md='4'> 
                     <br></br>
                     <h2>Login</h2>
@@ -32,32 +76,106 @@ export default function Home() {
                         <Form.Control type="password" placeholder="Password" />
                       </Form.Group>
                       <Button variant="primary" type="submit">
-                        Submit
+                        Log In
                       </Button>
                     </Form>    
                 </Col>
+                {/* REGISTER */}
                 <Col md='8'> 
                     <br></br>
                     <h2>Register</h2>
                     <Form>
-                      <Form.Group className="mb-3" controlId="formBasicEmail">
+                      <Form.Group className="mb-3" controlId="formRegisterBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control 
+                            type="email" 
+                            value={registerEmail}
+                            placeholder="Enter email" 
+                            onChange={(e) => setRegisterEmail(e.target.value)}
+                        />
                         <Form.Text className="text-muted">
                           We'll never share your email with anyone else.
                         </Form.Text>
                       </Form.Group>
-                      <Form.Group className="mb-3" controlId="formBasicPassword">
+                      <Form.Group className="mb-3" controlId="formRegisterBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control 
+                            type="password" 
+                            value={registerPassword}
+                            placeholder="Password" 
+                            onChange={(e) => setRegisterPassword(e.target.value)}
+                        />
+                      </Form.Group>
+                      <Form.Group className="mb-3" controlId="formPhoneNumber">
+                        <Form.Label>Phone Number</Form.Label>
+                        <Form.Control 
+                            type="tel" 
+                            value={registerPhoneNumber}
+                            placeholder="0123456789" 
+                            onChange={(e) => setRegisterPhoneNumber(e.target.value)}
+                        />
+                      </Form.Group>
+                      <Form.Group className="mb-3" controlId="formFirstName">
+                        <Form.Label>First Name</Form.Label>
+                        <Form.Control 
+                            type="text" 
+                            value={registerFirstName}
+                            placeholder="Satya" 
+                            onChange={(e) => setRegisterFirstName(e.target.value)}
+                        />
+                      </Form.Group>
+                      <Form.Group className="mb-3" controlId="formLastName">
+                        <Form.Label>Last Name</Form.Label>
+                        <Form.Control 
+                            type="text" 
+                            value={registerLastName}
+                            placeholder="Nadal" 
+                            onChange={(e) => setRegisterLastName(e.target.value)}
+                        />
+                      </Form.Group>
+                      <Form.Group className="mb-3" controlId="formRole">
+                        <Form.Label>Role</Form.Label>
+                        <Form.Control 
+                            as="select" 
+                            value={registerRole}
+                            onChange={(e) => setRegisterRole(e.target.value)}
+                        >
+                        <option value="C-Suite">C-Suite</option>
+                        <option value="Human Resources">Human Resources</option>
+                        <option value="Product Manager">Product Manager</option>
+                        <option value="Engineer">Engineer</option>
+                        <option value="Design">Design</option>
+                        <option value="Sales">Sales</option>
+                        <option value="Marketing">Marketing</option>
+                        </Form.Control>
+                      </Form.Group>
+                      <Form.Group className="mb-3" controlId="formBirthday">
+                        <Form.Label>Birthday</Form.Label>
+                        <Form.Control 
+                            type="date" 
+                            value={registerBirthday}
+                            placeholder="MM/DD/YYYY" 
+                            onChange={(e) => setRegisterBirthday(e.target.value)}
+                        />
+                      </Form.Group>
+                      <Form.Group className="mb-3" controlId="formAddress">
+                        <Form.Label>Address</Form.Label>
+                        <Form.Control 
+                            type="text" 
+                            value={registerAddress}
+                            placeholder="One Microsoft Way, Redmond, Washington 98052-6399"
+                            onChange={(e) => setRegisterAddress(e.target.value)}
+                        />
                       </Form.Group>
                       <Button variant="primary" type="submit">
-                        Submit
+                        Register
                       </Button>
                     </Form>    
+                    <br></br>
                 </Col>
             </Row>
         </Container>
+
     );
 }
 
