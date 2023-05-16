4b4a500443f7e11465cfd36c483979d187d5a392
Meeshbhoombah <rohanmishra2017@gmail.com>
2023-05-16 05:54:55 +0530

    Added static routing

--- src/main/java/demo/overlord/controller/HomeController.java
+++ src/main/java/demo/overlord/controller/HomeController.java
@@ -9,11 +9,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 class HomeController {
+
+	@RequestMapping(value = "/")
+	public String index() {
+		return "index";
+	}
 
 }
 
